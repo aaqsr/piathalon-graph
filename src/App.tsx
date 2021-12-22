b23c2360d3dce2e88a9b79c56f60b34013dc6b96
Awab <awab.qureshi@gmail.com>
2021-12-22 16:33:24 +0500

    Added MUI and slider

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from "react";
+import InputSlider from "./Slider";
 
 export const App:React.FC = () => (
    <>
+        <InputSlider />
    </>     
 )
---
+++ src/Slider.tsx
@@ -0,0 +1,65 @@
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import Slider from "@mui/material/Slider";
+import MuiInput from "@mui/material/Input";
+
+const Input = styled(MuiInput)`
+    width: 42px;
+`;
+
+export default function InputSlider() {
+    const [value, setValue] = React.useState<
+        number | string | Array<number | string>
+    >(30);
+
+    const handleSliderChange = (event: Event, newValue: number | number[]) => {
+        setValue(newValue);
+    };
+
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setValue(event.target.value === "" ? "" : Number(event.target.value));
+    };
+
+    const handleBlur = () => {
+        if (value < -10) {
+            setValue(10);
+        } else if (value > 10) {
+            setValue(10);
+        }
+    };
+
+    return (
+        <Box sx={{ width: 250 }}>
+            <Typography id="input-slider" gutterBottom>
+                Placeholder Text
+            </Typography>
+            <Grid container spacing={2} alignItems="center">
+                <Grid item xs>
+                    <Slider
+                        value={typeof value === "number" ? value : 0}
+                        onChange={handleSliderChange}
+                        aria-labelledby="input-slider"
+                    />
+                </Grid>
+                <Grid item>
+                    <Input
+                        value={value}
+                        size="small"
+                        onChange={handleInputChange}
+                        onBlur={handleBlur}
+                        inputProps={{
+                            step: 1,
+                            min: -10,
+                            max: 10,
+                            type: "number",
+                            "aria-labelledby": "input-slider"
+                        }}
+                    />
+                </Grid>
+            </Grid>
+        </Box>
+    );
+}
