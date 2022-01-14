8fe4d8733917923fd3a59375c2b31536c9d05780
Awab <awab.qureshi@gmail.com>
2022-01-15 00:52:38 +0500

    Multiple graph test

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 import Graph from './Graph';
 
-export const App:React.FC = () => (
-   <>
-        <Graph
-            func={"x^2"}
-            paramObj={{ "a": 0, "b": 0, "c": 0 }}
-        />
+export const App: React.FC = () => (
+    <>
+        <Graph func={"x^2"} paramObj={{ a: 0, b: 0, c: 0 }} />
 
-   </>     
-)
+        <Graph func={"x^2"} paramObj={{ a: 0, b: 0, c: 0 }} />
+    </>
+);
