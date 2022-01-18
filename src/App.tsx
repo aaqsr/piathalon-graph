3189f7e21a3916e09c3a91b057b4b2ef1ef27539
Awab <awab.qureshi@gmail.com>
2022-01-18 12:43:17 +0500

    Added test equations

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import Graph from './Graph';
 
 export const App: React.FC = () => (
     <>
-        {/* <Graph func={"{a} x^2 + {b} x + {c}"} paramObj={{ a: 0, b: 0, c: 0 }} /> */}
+        <Graph func={"a x^2 + b x + c"} paramObj={{ a: 1, b: 0, c: 0 }} />
 
-        <Graph func={"a sin( b x) + c"} paramObj={{ a: 0, b: 0, c: 0 }} />
+        <Graph func={"a sin( b x) + cx + d"} paramObj={{ a: 1, b: 1, c: 0, d: 0}} />
     </>
 );
