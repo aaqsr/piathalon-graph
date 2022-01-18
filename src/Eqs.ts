b7ee3bd2463cf6f2a864844e69a8c8351895546c
Awab <awab.qureshi@gmail.com>
2022-01-19 00:35:53 +0500

    Added equations

--- src/Eqs.ts
+++ src/Eqs.ts
@@ -9,16 +9,17 @@ export const trigs = {
     "y=sin(ax)": { a: 1 },
     "y=bsin(ax)": { a: 1, b: 1 },
     "y=atan(x)+b": { a: 1, b: 0 },
-    "y=d sin(e x)+ cos( f x)": { d: 1, e: 1, f: 0 },
 };
 
 export const exps = {
     "y=exp(ax)": { a: 1 },
-    "y=exp(ax)+b": { a: 1, b: 0 },
+    "y=exp(ax)+bx^2": { a: 1, b: 0 },
     "y=ln(ax)": { a: 1 },
-    "y=ln(ax)+b": { a: 1, b: 0 }
+    "y=ln(ax)+bx^2 +c": { a: 1, b: 0 }
 };
 
 export const final = {
-    "y=a tan(x)^2 + b tan(x) - c": { a: 1, b: 1, c: 0 },
+    "y=a exp(x) + b sin(x) + c": { a: 1, b: 1, c: 0 },
+    "y=a sin(b x)+  tan( c x)": { a: 1, b: 1, c: 0 },
+    "y=a x^3 + b sin (cx)":{a:1, b:0, c: 0}
 };
